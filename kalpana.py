05f8e96ec789184ad4eb34e4cf38128d23897fc4
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-03 16:21:07 +0530

    Delete kalpana.py

--- kalpana.py
+++
@@ -1,8 +0,0 @@
-ch=input()
-if(ch >= 'a'and ch<= 'z'):
-  if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-		print("Vowel")
-	else:
-            	print("Consonant")
-else:
-	print("invalid")
