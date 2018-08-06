944f2dbea5b5d42f27403b0830707f1e3160a1eb
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-06 15:11:30 +0530

    Create c.py

---
+++ c.py
@@ -0,0 +1,13 @@
+num=int(input())
+if(num<=1000):
+	if num > 1:
+		for i in range(2,num):
+			if (num % i) == 0:
+				print("no")
+			
+				break
+			
+		else:
+			 print("yes")
+	else:
+		 print("no")
