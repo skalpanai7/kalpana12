1621653a16393c5a96bd74f1e01945890f220a18
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-09 11:26:58 +0530

    Update b.py

--- b.py
+++ b.py
@@ -1,13 +1,12 @@
 n=int(input())
 temp=n
 rev=0
-if(n<=1000):
-	while(n>0):
-		k=n%10
-		rev=rev*10+k
-		n=n//10
-	if(temp==rev):
-		print("yes")
+while(n>0):
+	k=n%10
+	rev=rev*10+k
+	n=n//10
+if(temp==rev):
+	print("yes")
 		
-	else:
-		print("no")
+else:
+	print("no")
