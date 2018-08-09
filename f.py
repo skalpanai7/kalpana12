c82010f2b104c3c7f47e290a0822650065a77dee
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-09 13:17:38 +0530

    Update f.py

--- f.py
+++ f.py
@@ -2,13 +2,12 @@ n=int(input())
 temp=n
 l=len(str(n))
 sum=0
-if(n<=100000):
-	while n>0:
-		x=n%10
-		sum=sum+(x**l)
-		n=n/10	
-	if(sum==temp):
-		print("yes")
-	else:
-		print("no")
+while n>0:
+	x=n%10
+	sum=sum+(x**l)
+	n=n/10	
+if(sum==temp):
+	print("yes")
+else:
+	print("no")
 	
