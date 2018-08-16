13462737e3cad3c93d95be491ced7f31112048d2
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-16 15:28:11 +0530

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    if(n <= 1):
+        return n
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
+n = int(input("Enter number of terms:"))
+print("Fibonacci sequence:")
+for i in range(1,n+1):
+    print fibonacci(i),
