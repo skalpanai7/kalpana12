590d1c4930abe51f2f64375e48ba37f4ebb83f86
skalpanai7 <41998322+skalpanai7@users.noreply.github.com>
2018-08-16 15:30:55 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -3,7 +3,6 @@ def fibonacci(n):
         return n
     else:
         return(fibonacci(n-1) + fibonacci(n-2))
-n = int(input("Enter number of terms:"))
-print("Fibonacci sequence:")
+n = int(raw_input())
 for i in range(1,n+1):
     print fibonacci(i),
